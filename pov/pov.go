df53bc6ea80e27c68c21126bbe08540da373200f
jpmondet <jp@mondet.org>
2018-11-10 21:44:29 +0100

    pov exercise

--- pov/pov.go
+++ pov/pov.go
@@ -4,19 +4,10 @@ import (
 	"fmt"
 )
 
-type Arc struct {
-	fr string
-	to string
-}
-
-type Node struct {
-	name string
-}
-
 type Graph struct {
-	nodes []Node
-	arcs  []Arc
-	root  string
+	nodes map[string]int
+	names []string
+	arcs  [][]int
 }
 
 func New() *Graph {
@@ -27,110 +18,132 @@ func New() *Graph {
 	return &g
 }
 
-func (g *Graph) AddNode(l string) {
-	if g.nodes == nil {
-		g.nodes = make([]Node, 1)
-		g.nodes[0] = Node{name: l}
-		g.root = l
-	} else {
-		n := Node{name: l}
-		g.nodes = append(g.nodes, n)
+func (g *Graph) nodeExist(l string) bool {
+	if _, ok := g.nodes[l]; !ok {
+		return false
 	}
+	return true
 }
 
-func (g *Graph) AddArc(from, to string) {
-	if g.arcs == nil {
-		g.arcs = make([]Arc, 1)
-		g.arcs[0] = Arc{fr: from, to: to}
-	} else {
-		a := Arc{fr: from, to: to}
-		g.arcs = append(g.arcs, a)
-	}
-}
-
-func (g *Graph) ArcList() []string {
-	var s []string
-	for _, arc := range g.arcs {
-		arcFmt := fmt.Sprintf("%s -> %s", arc.fr, arc.to)
-		s = append(s, arcFmt)
-	}
-	return s
-}
-
-func (g *Graph) NodeArcs(node string) (arcNode []Arc) {
-	for _, arc := range g.arcs {
-		if arc.fr == node || arc.to == node {
-			arcNode = append(arcNode, arc)
+func (g *Graph) getParents(l string) (parents []int) {
+	nodeIndex := g.nodes[l]
+	for frIndex, tto := range g.arcs {
+		for _, to := range tto {
+			if to == nodeIndex {
+				parents = append(parents, frIndex)
+			}
 		}
 	}
-	return arcNode
+	return parents
 }
 
-func (g *Graph) FindNode(node string) Node {
-	for _, n := range g.nodes {
-		fmt.Println(n)
-		if n.name == node {
-			return n
-		}
-	}
-	return Node{name: ""}
-}
-
-func (g *Graph) Childs(node string) (childs []Node) {
-	arcs := g.NodeArcs(node)
-	for _, arc := range arcs {
-		if arc.to != node {
-			child := g.FindNode(arc.to)
-			childs = append(childs, child)
-			fmt.Println(child, childs)
-		}
+func (g *Graph) getChilds(l string) (childs []int) {
+	nodeIndex := g.nodes[l]
+	childs = make([]int, len(g.names))
+	for _, to := range g.arcs[nodeIndex] {
+		childs = append(childs, to)
 	}
 	return childs
 }
 
-func Rearrange(node string, g *Graph, g2 *Graph, parent string) (*Graph, *Graph) {
-	var neigh string
-	n := g2.FindNode(node)
-	if n.name == "" {
-		g2.AddNode(node)
-		arcs := g.NodeArcs(node)
-		for _, arc := range arcs {
-			if parent == "" { // We are NewRoot
-				if arc.to == node {
-					arc.to = arc.fr
-					arc.fr = node
-				}
-				g2.AddArc(arc.fr, arc.to)
-				g, g2 = Rearrange(arc.to, g, g2, node)
-			} else { // We are a child
-				if arc.to != node {
-					neigh = arc.to
-				} else {
-					neigh = arc.fr
-				}
-				neighNode := g2.FindNode(neigh)
-				if neighNode.name == "" {
-					if arc.to == parent {
-						arc.to = arc.fr
-						arc.fr = parent
-					}
-					g2.AddArc(arc.fr, arc.to)
-				}
-			}
-		}
-		childs := g.Childs(node)
-		for _, child := range childs {
-			g, g2 = Rearrange(child.name, g, g2, node)
-		}
+func (g *Graph) AddNode(l string) {
+	if g.nodes == nil {
+		g.names = append(g.names, l)
+		m := make(map[string]int)
+		m[l] = 0
+		g.nodes = m
+	} else {
+		g.nodes[l] = len(g.names)
+		g.names = append(g.names, l)
 	}
-	return g, g2
 }
 
+func (g *Graph) AddArc(from, to string) {
+	var frIndex, toIndex int
+	var ok bool
+	if g.arcs == nil {
+		g.arcs = make([][]int, 100)
+	}
+	if frIndex, ok = g.nodes[from]; !ok {
+		g.AddNode(from)
+	}
+	if toIndex, ok = g.nodes[to]; !ok {
+		panic("Wrong test case...")
+	}
+	frIndex = g.nodes[from]
+	toIndex = g.nodes[to]
+	g.arcs[frIndex] = append(g.arcs[frIndex], toIndex)
+}
+
+func (g *Graph) ArcList() []string {
+	var s []string
+	for frIndex, tto := range g.arcs {
+		for _, to := range tto {
+			arcFmt := fmt.Sprintf("%s -> %s", g.names[frIndex], g.names[to])
+			s = append(s, arcFmt)
+		}
+	}
+	return s
+}
+
+/*
 func (g *Graph) ChangeRoot(root, newRoot string) *Graph {
-	//TODO
+	var swapped bool
 	g2 := New()
-	g, g2 = Rearrange(newRoot, g, g2, "")
+	for k, _ := range g.nodes {
+		g2.AddNode(k)
+	}
+	for frIndex, tto := range g.arcs {
+		fmt.Println("ChangeRoot_g2: ", g2)
+		if frIndex != g.nodes[newRoot] {
+			fmt.Println("ChangeRoot_NotNewRoot: ", g2)
+			swapped = false
+			childs := g.getChilds(g.names[frIndex])
+			for child := range childs {
+				if child == g.nodes[newRoot] {
+					swapped = true
+				}
+			}
+			if swapped {
+				g2.AddArc(newRoot, g.names[frIndex])
+				for child := range childs {
+					if child != g.nodes[newRoot] {
+						g2.AddArc(newRoot, g.names[child])
+					}
+				}
+			}
+		} else {
+			fmt.Println("ChangeRoot_NewRoot: ", g2)
+			for _, to := range tto {
+				g2.AddArc(newRoot, g.names[to])
+			}
+		}
+	}
+
 	return g2
+}*/
+
+func (g *Graph) ChangeRoot(oldRoot, newRoot string) *Graph {
+	nr := g.nodes[newRoot]
+	var f func(int) bool
+	f = func(n int) (found bool) {
+		if n == nr {
+			return true
+		}
+		a := g.arcs[n]
+		for i, to := range a {
+			if f(to) {
+				last := len(a) - 1
+				a[i] = a[last]
+				g.arcs[n] = a[:last]
+				g.arcs[to] = append(g.arcs[to], n)
+				return true
+			}
+		}
+		return false
+	}
+	f(g.nodes[oldRoot])
+	return g
 }
 
 func main() {
